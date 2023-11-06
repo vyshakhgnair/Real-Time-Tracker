d9c21d798115079c99c1e6882ae0633993b23cd4
Shiras Rahman U <84281609+Shiras-U@users.noreply.github.com>
2023-11-06 16:50:20 +0530

    Update upload.js

--- real-tracker/src/upload.js
+++ real-tracker/src/upload.js
@@ -41,16 +41,21 @@ function FileUpload() {
   return (
     <div>
       <h1> File Upload</h1>
+      <div className="modal-content7">
       <div>
         <label>Enter the client ID:</label>
-        <input type="text" value={clientId} onChange={handleClientIdChange} />
+        <input type="text" className="modal-box7" value={clientId} onChange={handleClientIdChange} />
       </div>
       <div>
         <label>Enter the OA Number:</label>
-        <input type="text" value={OANumber} onChange={handleOANumberChange} />
+        <input type="text" className="modal-box7" value={OANumber} onChange={handleOANumberChange} />
       </div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <br/><br/>
+      <input type="file" id='filebutton' onChange={handleFileChange} />
+      <br/><br/><br/>
+      <button className="logout7" onClick={handleSubmit}>Submit</button>
+      
     </div>
   );
 }
