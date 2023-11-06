bcd4766f4bb53d1b7ad9bc4e4402d636eb26e70a
Shiras Rahman U <84281609+Shiras-U@users.noreply.github.com>
2023-11-06 11:25:22 +0530

    Add files via upload

---
+++ real-tracker/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CustomerDashBoard from './customers';
+import ColorTiles from './ColorTiles';
+import UserDashBoard from './users';
+import Login from './login';
+import ClientViewer from './clientViewer';
+import Tiles from './tile';
+import FileUpload from './fileup';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/customers" element={<CustomerDashBoard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/users" element={<UserDashBoard />} />
+        <Route path="/tile/:OAN" element={<Tiles />} />
+        <Route path="/ColorTiles" element={<ColorTiles />} />
+        <Route path="/upload" element={<FileUpload />} />
+        <Route path="/clientViewer/:OAN" element={<ClientViewer />} />
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
